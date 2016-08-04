52c3f6025ca9373420f2e2df0aeea9431f5006a1
Andrew Turner <andrewjamesturner0@gmail.com>
2016-08-04 17:29:14 +0100

    Add setup.sh to install R

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+if [ $(grep -c '^deb http://www.stats.bris.ac.uk/R/bin/linux/ubuntu trusty/' /etc/apt/sources.list) -eq 0 ]; then
+    sh -c 'echo "deb http://www.stats.bris.ac.uk/R/bin/linux/ubuntu trusty/" >> /etc/apt/sources.list'
+    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
+fi
+
+apt-get update
+apt-get install -y \
+    r-base \
+    r-base-dev \
+    libcurl4-openssl-dev \
+    libxml2-utils \
+    libxml2-dev \
+    vim \
+    git
+
+Rscript -e \
+    "options(repos=c(CRAN='http://cran.rstudio.com', \
+                          'http://cran.obiba.org')); \
+     install.packages(c('rjson', \
+                        'RCurl', \
+                        'devtools', \
+                        'opal', \
+                        'opaladmin', \
+                        'roxygen2'))"
