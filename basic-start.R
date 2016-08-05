be0f033a74e87672983e4fd3a570c2cd6b7cc1ba
Andrew Turner <andrewjamesturner0@gmail.com>
2016-08-05 17:12:30 +0100

    Add logindata and simple startup script

---
+++ basic-start.R
@@ -0,0 +1,13 @@
+library(opal)
+library(opaladmin)
+library(devtools)
+
+#load_all('dsClient')
+#load_all('dsBaseClient')
+#load_all('dsStatsClient')
+#load_all('dsGraphicsClient')
+#load_all('dsModellingClient')
+
+
+logins <- read.csv('logindata/vagrant.datashield_testdata_ubuntu.DASIM.csv')
+opals <- datashield.login(logins=logins, assign=TRUE)
